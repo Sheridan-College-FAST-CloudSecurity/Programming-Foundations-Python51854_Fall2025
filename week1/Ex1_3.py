61aaa965020f556d252af44805bad13703bfef9a
profpallavi <119691186+profpallavi@users.noreply.github.com>
2025-09-05 14:56:21 -0400

    Add files via upload

---
+++ week1/Ex1_3.py
@@ -0,0 +1,38 @@
+BONUS=100 #Constant
+employeeName ="cynthia" #String Variable
+employeeId=1212121 #Integer variable
+noOfHours=40.0#float
+rate=90.5 #float
+salary=noOfHours*rate #Arithmatic Operator *
+print(salary)
+salary=salary+BONUS #Arithmatic Operator +
+print(salary)
+salary=salary-50 #Arithmatic Operator -
+
+num1=15 #Assignment Operator =
+num2=2
+result=num1%num2 #Arithmatic operator % modulus returns reminder
+print(result)
+result=num1//num2 #Arithmatic operator floor division
+print(result)
+result=num1**num2 #Arithmatic operator ** exponential
+print(result)
+result=num1/num2 #Arithmatic operator /
+print(result)
+salary-=50 #Assignmet operator -=
+print(salary)
+salary+=50 
+print(salary) #Assignmet operator += ,*= ,/= for all arithmatic operators
+print(num1==num2) #Comparison operator
+print(num1>num2) #Comparison operator
+print(num1<num2) #Comparison operator
+print(num1>=num2) #Comparison operator
+print(num1<=num2) #Comparison operator
+print(num1>10 and num1<20)
+ #Logical and operator returns true if both conditions are true
+print(num1<10 or num1<13)
+ #Logical OR operator returns true if one of the conditions are true
+print(not(num1>10 and num1<20))
+print(not(num1<10 or num1<13))
+ #Logical Not operator negates the result
+ 
---
+++ week1/Ex1_4.py
@@ -0,0 +1,20 @@
+PI=3.14
+print("Area of the circle")
+r=float(input("Enter radius:"))
+area=3.14*r*r
+print(area)
+print("Area of the rectangle")
+
+l=float(input("Enter length"))
+w=float(input("Enter width"))
+area=w*l
+print(area)
+print("Area of the Tria1gle")
+
+h=float(input("Enter height"))
+b=float(input("Enter base"))
+area=0.5*h*b
+print(f"Area of the triagle {area}") #f String literals(Formatted String)
+s=int(input("Enter number"))
+total=s+l+b
+print(total)
